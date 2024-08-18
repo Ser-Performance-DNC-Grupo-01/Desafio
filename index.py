a13799378bff2591ace2d36e61ab3160965dde23
Mackson Douglas <136249879+DouglasRm2@users.noreply.github.com>
2024-08-18 20:41:05 -0300

    Atualização 18-08-2024 - 20:40

--- index.py
+++ index.py
@@ -4,27 +4,54 @@ import numpy as np
 import os
 import seaborn as sns
 import matplotlib.pyplot as plt
-import statsmodels.formula.api as smf
 import plotly.express as px
+import plotly.graph_objects as go
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from sklearn.decomposition import PCA
-from sklearn.preprocessing import scale, LabelEncoder, MinMaxScaler
-from sklearn.cluster import KMeans
+from sklearn.preprocessing import scale, LabelEncoder
+from sklearn.cluster import KMeans, DBSCAN, MeanShift, AgglomerativeClustering
 from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_harabasz_score
 from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 from kmodes.kmodes import KModes
 from kmodes.kprototypes import KPrototypes
+import statsmodels.formula.api as smf
+from PIL import Image
+st.set_page_config(layout="wide")
 
-# Configuração do tema do Seaborn
-sns.set_theme(
-    context="talk",
-    style="darkgrid",
-    rc={
-        'figure.figsize': (8, 4),
-    },
-)
+with open("D:\PROJETO_REAL_DNC_-SER-PERFORMACE\git\style.css") as g:
+    st.markdown(f"<style>{g.read()}</style>", unsafe_allow_html= True)
+
+
+logo_path = r'D:\PROJETO_REAL_DNC_-SER-PERFORMACE\git\logo.png'  
+logo = Image.open(logo_path)
+st.image(logo, width=300)
+st.markdown("""
+   
+    <style>
+    
+    .st-emotion-cache-1itdyc2.eczjsme18{
+        
+        box-shadow: 0 4px 8px rgb(241, 133, 10); 
+    }
+    .logo {
+        margin-right: 20px;
+    }
+    h1{
+        color: #ec9509;
+        font-size: 50px;
+        text-align: center;
+    }
+    </style>
+    <div class="head">
+        <h1>Análise de Dados de Clientes</h1>
+    </div>
+""", unsafe_allow_html=True)
+
+
+col1, col2, col3 = st.columns(3)
+
 
 # Função para carregar e tratar dados
 def carregar_dados(caminho, pasta_selecionada):
@@ -94,85 +121,107 @@ def plotar_grafico_3d(vendas, kmeans_labels):
 def pagina_acessos(dados):
     if 'acessos' in dados:
         acessos = tratar_dados_nulos(dados['acessos'])
-        st.subheader("Acessos por Funcionário")
-        funcacessos = acessos.groupby('Funcionario')['Quantidade_de_Acessos'].sum().sort_values(ascending=False)
-        st.bar_chart(funcacessos.head(10))
         
-        st.subheader("Acessos por Unidade")
-        unidade = acessos.groupby('Unidade')['Quantidade_de_Acessos'].sum().sort_values(ascending=False)
-        st.bar_chart(unidade.head(10))
+        # Criação das colunas
+        col1, col2 = st.columns([1, 1])  # Define o espaço igual para as colunas
+
+        # Adicionar gráficos nas colunas
+        with col1:
+           
+            st.subheader("Acessos por Funcionário")
+            funcacessos = acessos.groupby('Funcionario')['Quantidade_de_Acessos'].sum().sort_values(ascending=False)
+            fig1 = go.Figure(data=go.Bar(
+                x=funcacessos.head(10).index, 
+                y=funcacessos.head(10),
+                marker_color='orange',
+                text=[],  
+                textposition='none'  
+            ))
+            fig1.update_layout(
+                paper_bgcolor='#0E1117',
+                plot_bgcolor='#0E1117',
+                title='',
+                title_font_color='orange',
+                xaxis_title='',
+                yaxis_title='Quantidade de Acessos',
+                xaxis_title_font_color='orange',
+                yaxis_title_font_color='orange',
+                xaxis_tickfont_color='orange',
+                yaxis_tickfont_color='orange',
+                margin=dict(l=40, r=20, t=40, b=40),
+                showlegend=False  # Remove a legenda
+            )
+            st.plotly_chart(fig1, use_container_width=True)
+
+        with col2:
+            st.subheader("Acessos por Unidade")
+            unidade = acessos.groupby('Unidade')['Quantidade_de_Acessos'].sum().sort_values(ascending=False)
+            fig2 = go.Figure(data=go.Bar(x=unidade.head(10).index, y=unidade.head(10), marker_color='orange'))
+            fig2.update_layout(
+                paper_bgcolor='#0E1117',
+                plot_bgcolor='#0E1117',
+                title='',
+                title_font_color='orange',
+                xaxis_title='',
+                yaxis_title='Quantidade de Acessos',
+                xaxis_title_font_color='orange',
+                yaxis_title_font_color='orange',
+                xaxis_tickfont_color='orange',
+                yaxis_tickfont_color='orange',
+                margin=dict(l=40, r=20, t=40, b=40)
+            )
+            st.plotly_chart(fig2, use_container_width=True)
+
     else:
         st.error("Dados de acessos não encontrados.")
-        
-    st.write("Colunas disponíveis:", acessos.columns)
 
 # Função para a página de análises de vendas
 def pagina_vendas(dados):
     if 'vendas' in dados:
         vendas = tratar_dados_nulos(dados['vendas'])
         
-        # Garantir que 'Quantidade_de_Acessos' esteja presente
         if 'Quantidade_de_Acessos' not in vendas.columns:
             if 'Acessos' in vendas.columns:
                 vendas['Quantidade_de_Acessos'] = vendas['Acessos']
             else:
                 vendas['Quantidade_de_Acessos'] = 0
-
-        # Criar uma coluna de devoluções
+        
         vendas['Devolucoes'] = vendas.apply(lambda row: 'S' if pd.isna(row['N_Produtos']) or row['N_Produtos'] <= 0 else 'N', axis=1)
 
-        # Seções de Análise
-        st.title("Análise de Vendas")
+        st.subheader("Ticket Médio")
+        tcktmedio = vendas['Vlr_Bruto'].sum() / vendas.shape[0]
+        st.write(f'Ticket médio: R${tcktmedio:.2f}')
 
-        # Tabela de Estatísticas Resumidas
-        st.header("Estatísticas Resumidas")
-        estatisticas = pd.DataFrame({
-            'Métrica': ['Ticket Médio', 'Faturamento Bruto', 'Faturamento Líquido', 'Total de Descontos', 'Total de Produtos Vendidos'],
-            'Valor': [
-                f"R${vendas['Vlr_Bruto'].sum() / vendas.shape[0]:.2f}",
-                f"R${vendas['Vlr_Bruto'].sum():.2f}",
-                f"R${vendas['Vlr_Liquido'].sum():.2f}",
-                f"R${vendas['Vlr_Desconto'].sum():.2f}",
-                f"{vendas['N_Produtos'].sum():.0f} unidades"
-            ]
-        })
-        st.dataframe(estatisticas, use_container_width=True)
+        st.subheader("Faturamento Bruto")
+        FatBruto = vendas['Vlr_Bruto'].sum()
+        st.write(f'Faturamento bruto: R${FatBruto:.2f}')
 
-        # Top 10 Funcionários que Mais Venderam
-        st.header("Top 10 Funcionários que Mais Venderam")
-        func_rank = vendas['Funcionario'].value_counts().head(10)
-        st.bar_chart(func_rank, use_container_width=True)
+        st.subheader("Faturamento Líquido")
+        fatliq = vendas['Vlr_Liquido'].sum()
+        st.write(f'Faturamento líquido: R${fatliq:.2f}')
 
-        # Funcionários com Menos Vendas
-        st.header("Funcionários com Menos Vendas")
-        func_rank_tail = vendas['Funcionario'].value_counts().tail(10)
-        st.bar_chart(func_rank_tail, use_container_width=True)
+        st.subheader("Total de Descontos")
+        total_descontos = vendas['Vlr_Desconto'].sum()
+        st.write(f'Total de descontos: R${total_descontos:.2f}')
 
-        # Devoluções
-        st.header("Devoluções")
+        st.subheader("Total de Produtos Vendidos")
+        totprod = vendas['N_Produtos'].sum()
+        st.write(f'Total de produtos: {totprod:.0f} unidades')
+
+        st.subheader("Top 10 Funcionários que Mais Venderam")
+        func_rank = vendas['Funcionario'].value_counts()
+        top10func = func_rank.head(10)
+        st.bar_chart(top10func)
+
+        st.subheader("Funcionários com Menos Vendas")
+        tail10func = func_rank.tail(10)
+        st.bar_chart(tail10func)
+
+        st.subheader("Devoluções")
         devolucao = vendas['Devolucoes'].value_counts()
         st.write(devolucao)
-
-        # Visualizações detalhadas
-        st.header("Visualizações Detalhadas")
-
-        # Distribuição do Faturamento Líquido
-        st.subheader("Distribuição do Faturamento Líquido")
-        plt.figure()
-        sns.histplot(vendas['Vlr_Liquido'], kde=True)
-        plt.title('Distribuição do Faturamento Líquido')
-        plt.xlabel('Faturamento Líquido')
-        plt.ylabel('Frequência')
-        st.pyplot(plt)
-
-        # Correlação entre Variáveis
-        st.subheader("Correlação entre Variáveis")
-        plt.figure()
-        corr = vendas[['Vlr_Bruto', 'Vlr_Desconto', 'Vlr_Liquido', 'N_Produtos']].corr()
-        sns.heatmap(corr, annot=True, cmap='coolwarm', fmt='.2f')
-        plt.title('Matriz de Correlação')
-        st.pyplot(plt)
         
+        st.write("Colunas disponíveis:", vendas.columns)
     else:
         st.error("Dados de vendas não encontrados.")
 
@@ -191,13 +240,9 @@ def pagina_clustering(dados):
             vendas['Devolucoes'] = vendas['Devolucoes'].map({'S': 1, 'N': 0})
         if 'Treinamento' in vendas.columns:
             vendas['Treinamento'] = vendas['Treinamento'].fillna('N').map({'S': 1, 'N': 0})
-    
 
         vendas = vendas.select_dtypes(include=[np.number])
         vendas = vendas.fillna(0)
-
-        numerical_columns = ['N_Produtos', 'Vlr_Bruto', 'Vlr_Liquido', 'Vlr_Desconto']
-        vendas[numerical_columns] = vendas[numerical_columns].apply(scale)
 
         st.subheader("Métricas de Clustering")
         cluster_metrics = calcular_metricas_cluster(vendas)
@@ -221,7 +266,7 @@ def pagina_regressao(dados):
         # Definindo a fórmula para o modelo de regressão linear
         function = 'Vlr_Liquido ~ Vlr_Bruto + Vlr_Desconto + N_Boletos + N_Produtos - 1'
         
-        # Ajustando o modelo de regressão linear com statsmodels
+        # Ajustando o modelo de regressão linear
         model = smf.ols(formula=function, data=vendas).fit()
         
         # Exibindo o resumo do modelo
@@ -232,12 +277,8 @@ def pagina_regressao(dados):
         x = vendas[['Vlr_Bruto', 'Vlr_Desconto', 'N_Boletos', 'N_Produtos']]
         y = vendas['Vlr_Liquido']
         
-        # Aplicando Min-Max Scaling
-        scaler = MinMaxScaler()
-        x_scaled = scaler.fit_transform(x)
-        
         # Separando os dados de treino e teste
-        x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=42)
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
         
         # Criando o objeto de Regressão Linear
         lr = LinearRegression()
@@ -245,13 +286,11 @@ def pagina_regressao(dados):
         # Treinando o modelo
         lr.fit(x_train, y_train)
         
-        # Calculando o coeficiente de determinação (R²) para os dados de treino e teste
-        r_sq_train = lr.score(x_train, y_train)
-        r_sq_test = lr.score(x_test, y_test)
+        # Calculando o coeficiente de determinação (R²)
+        r_sq = lr.score(x, y)
         
         # Exibindo o coeficiente de determinação
-        st.write(f'Coeficiente de Determinação (R²) - Treinamento: {r_sq_train:.2f}')
-        st.write(f'Coeficiente de Determinação (R²) - Teste: {r_sq_test:.2f}')
+        st.write(f'Coeficiente de Determinação (R²): {r_sq:.2f}')
         
         # Previsões e métricas de erro para dados de treinamento
         y_pred_train = lr.predict(x_train)
@@ -260,22 +299,12 @@ def pagina_regressao(dados):
         st.write(f'RMSE (Treinamento): {np.sqrt(metrics.mean_squared_error(y_train, y_pred_train)):.2f}')
         
         # Previsões e métricas de erro para dados de teste
-        y_pred_test = lr.predict(x_test)
-        st.write(f'MAE (Teste): {metrics.mean_absolute_error(y_test, y_pred_test):.2f}')
-        st.write(f'MSE (Teste): {metrics.mean_squared_error(y_test, y_pred_test):.2f}')
-        st.write(f'RMSE (Teste): {np.sqrt(metrics.mean_squared_error(y_test, y_pred_test)):.2f}')
+        y_pred = lr.predict(x_test)
+        st.write(f'MAE (Teste): {metrics.mean_absolute_error(y_test, y_pred):.2f}')
+        st.write(f'MSE (Teste): {metrics.mean_squared_error(y_test, y_pred):.2f}')
+        st.write(f'RMSE (Teste): {np.sqrt(metrics.mean_squared_error(y_test, y_pred)):.2f}')
         
-        # Visualização das previsões vs valores reais
-        st.subheader("Visualização das Previsões vs Valores Reais")
-        plt.figure()
-        plt.scatter(y_test, y_pred_test, alpha=0.3)
-        plt.xlabel('Valores Reais')
-        plt.ylabel('Previsões')
-        plt.title('Previsões vs Valores Reais')
-        plt.grid(True)
-        st.pyplot(plt)
-        
-        # Pairplot das variáveis
+        # Exibindo o pairplot das variáveis
         st.subheader("Pairplot")
         sns_plot = sns.pairplot(vendas[['Vlr_Bruto', 'Vlr_Desconto', 'Vlr_Liquido', 'N_Produtos']])
         st.pyplot(sns_plot.figure)
@@ -284,9 +313,11 @@ def pagina_regressao(dados):
 
 # Função principal do Streamlit
 def main():
-    st.title("Análise de Dados de Clientes")
-
-    caminho = st.text_input("Digite o caminho do diretório:", r'/home/nickson/DNC-Projetos/Ser_Performance/Clientes')
+    
+    
+    
+    # caminho = st.text_input("Digite o caminho do diretório:", r'D:\PROJETO_REAL_DNC_-SER-PERFORMACE\clientes_EMPRESA-SER')
+    caminho =  r'D:\PROJETO_REAL_DNC_-SER-PERFORMACE\clientes_EMPRESA-SER'
     
     if caminho:
         try:
@@ -294,7 +325,7 @@ def main():
             pastas = [p for p in os.listdir(caminho) if os.path.isdir(os.path.join(caminho, p))]
             
             if pastas:
-                pasta_selecionada = st.selectbox("Escolha uma pasta:", pastas)
+                pasta_selecionada = col1.selectbox("Escolha uma pasta:", pastas)
                 cliente = os.path.join(caminho, pasta_selecionada)
 
                 # Carregar os dados da pasta selecionada
